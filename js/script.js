b072cda92f85b3c457d172d38e2d8affbb650060
Darius104 <comandarius@gmail.com>
2021-12-03 16:17:11 +0100

    Commenti aggiunti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 Vue.config.devtools = true;
+
 const app = new Vue({
     el: '#root',
     data: {
+        // variabile impostata a 0 per poter collegare le Thumbs con le immagini
         activeSlide: 0,
-
+        // creo il mio array di oggetti
         slides: 
         [
             {
@@ -34,11 +36,16 @@ const app = new Vue({
         ]
     },
     methods:{
+        // aggiungo i metodi per le arrow
+        // arrow next controllo se la variabile inizializzata a 0 è minore della lunghezza dell'array - 1 vado a
+        // incrementare il numero
         nextSlide: function(){
             if( this.activeSlide < this.slides.length - 1 ){
                 this.activeSlide++;
             }
         },
+        // arrowB controllo se la variabile inizializzata a 0 è maggiore di 0 vado a
+        // decrementare il numero
         bSlide: function(){
             if( this.activeSlide > 0 ){
                 this.activeSlide--;
